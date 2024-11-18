6e52d57bcd790eb1366d6cebed9753f1ff4edb4e
kunchamMani16 <112928235+kunchamMani16@users.noreply.github.com>
2024-11-18 10:56:24 +0530

    Create src/test/java/com/example/utils/Utilities.java

---
+++ src/test/java/com/example/utils/Utilities.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Utilities {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public Utilities() {
+        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public void openWebsite(String url) {
+        driver.get(url);
+    }
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void clickElement(By locator) {
+        findElement(locator).click();
+    }
+
+    public void enterText(By locator, String text) {
+        findElement(locator).sendKeys(text);
+    }
+
+    public void waitForElementVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
